cb856c5abde02967bb9fa2d4db45ad6410ebf1b6
kal200 <kalippert35@gmail.com>
2026-03-10 15:32:07 -0400

    initial commit, basic login page, user routes, DB connection (#1)

---
+++ app/server/routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+//get
+router.get("/", async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+export default router;
+
+//post
+router.post("/", async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+});
