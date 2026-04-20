05591654bcadc7ac8dbfc995c8b356fb23d7dbd0
Seth Cruz <sethcruz@Seths-MacBook-Pro.local>
2026-04-20 11:24:38 -0400

    Community page fixes

--- app/client/src/pages/Community.jsx
+++ app/client/src/pages/Community.jsx
@@ -281,11 +281,6 @@ export default function CommunityPage() {
           </div>
         </div>
       </div>
-export default function CommunityPage() {
-  return (
-    <div className="community-page">
-      <h1>Community</h1>
-      <p>Connect with other users and grow your network.</p>
     </div>
   );
 }
