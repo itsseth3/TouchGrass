58f717c750fd866e80d5d47f9ce4bb676affc422
kal200 <kalippert35@gmail.com>
2026-04-20 12:49:06 -0400

    added routes for FindFriends, post creator/editor, masked password in register

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -7,6 +7,9 @@ import Activities from "./pages/Activities";
 import SettingsPrefs from "./pages/SettingsPrefs";
 import CommunityPage from "./pages/Community";
 import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import ViewPost from "./pages/ViewPost";
+import FindFriends from "./pages/FindFriends";
 
 function App() {
     return (
@@ -16,8 +19,11 @@ function App() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/activities" element={<Activities />} />
                 <Route path="/posts/create" element={<CreatePost />} />
+                <Route path="/posts/:postId" element={<ViewPost />} />
+                <Route path="/posts/:postId/edit" element={<EditPost />} />
                 <Route path="/settingsandpreferences" element={<SettingsPrefs />} />
                 <Route path="/community" element={<CommunityPage />} />
+                <Route path="/findfriends" element={<FindFriends />} />
             </Routes>
         </BrowserRouter>
     );
